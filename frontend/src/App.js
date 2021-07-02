95f42ef8d6837fe2838589a5a13a74f9475163a4
yasintorun <yasintorun4680@gmail.com>
2021-07-02 14:58:18 +0300

    sayfalama yapısı eklendi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,16 +18,12 @@ function App() {
       <ToastContainer position="top-right" autoClose={2500} hideProgressBar={true} pauseOnHover={false} />
 
       <Switch>
+        <Route exact path="/" component={HomeDashboard} />
         <Route exact path="/admin*" component={AdminDashboard} />
-
-        <Route exact path="/resume*" component={ResumeDetail} />
         <Route exact path="/jobseeker_dashboard*" component={JobseekerDashboard} />
         <Route exact path="/employer_dashboard*" component={EmployerDashboard} />
-        
         <Route path={Links.USERS} component={Auth} />
-        <Route exact path="/" component={HomeDashboard} />
         <Route path="/" component={Dashboard} />
-        
       </Switch>
     </div>
   );
